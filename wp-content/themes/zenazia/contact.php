b89b4a5b930ad51d4fb780efb5d5ea27835b15cc
Khwaj <philippe.roquesgeoffroy@gmail.com>
2021-03-28 10:50:37 +0200

    Added contact page + style

--- wp-content/themes/zenazia/contact.php
+++ wp-content/themes/zenazia/contact.php
@@ -6,9 +6,8 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<?php 
-		echo do_shortcode('[contact-form-7 id="29" title="Formulaire de contact"]', false);
-
 		//appel de ACF pour le contenu
+		echo do_shortcode('[contact-form-7 id="29" title="Formulaire de contact"]', false);
 		?>
 
 	</main><!-- #main -->
