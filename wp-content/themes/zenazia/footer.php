c49022e5bf3734c94750ef1e7d11e5aef25186a3
Gilles <gilles.ferrand31@gmail.com>
2021-04-01 22:25:29 +0200

    Accueil v2

--- wp-content/themes/zenazia/footer.php
+++ wp-content/themes/zenazia/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,14 +12,71 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-		    <p><?php echo the_field("footer_copyright") . ' ' . current_time('Y'); ?></p>
-   		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer">
+	<button id="scrollToTop">^</button>
+	<div class="site-info">
+		<div class="upperFooter">
+			<div class="menu-footer">
+				<h3>Menu</h3>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'footer-menu',
+					)
+				);
+				?>
+			</div>
+			<div class="contact">
+				<h3>Nous contacter</h3>
+				<div class="contactBalma">
+					<p>Balma</p>
+					<a href="tel:<?php echo get_field("telephone_balma_reel"); ?>">Téléphone : <?php echo get_field("telephone_balma"); ?></a>
+				</div>
+				<div class="contactQuint">
+					<p>Quint Fonsegrives</p>
+					<a href="tel:<?php echo get_field("telephone_quint_reel"); ?>">Téléphone : <?php echo get_field("telephone_quint"); ?></a>
+				</div>
+			</div>
+			<div class="outro">
+				<h3>Notre service</h3>
+				<p>Notre cuisine est élaborée sur place, tout en harmonisant, la matière première, les épices, rhizomes&herbes fraîches aromatiques et médicinales. (sans glutamate).</p>
+				<p>Nos plats sont poêlés au wok dès la prise de commadne pour vos repas ou à emporter. (la liste est à votre disposition)</p>
+			</div>
+		</div>
+		<!-- <div class="socialMedia">
+			<a href="https://www.facebook.com/Zenazia-Toulouse-Balma-152493081450568">
+				<svg width="25" height="25" viewBox="0 0 25 25">
+					<path d="M13.353 25H1.38A1.38 1.38 0 010 23.62V1.38C0 .618.618 0 1.38 0h22.24C24.382 0 25 .618 25 1.38v22.24A1.38 1.38 0 0123.62 25h-6.37v-9.681h3.25l.486-3.773H17.25v-2.41c0-1.092.303-1.836 1.87-1.836h1.997V3.924c-.345-.046-1.531-.15-2.911-.15-2.88 0-4.853 1.76-4.853 4.988v2.783h-3.258v3.773h3.258V25z" fill="#FFF" fill-rule="evenodd"></path>
+				</svg>
+				<span>Balma</span>
+			</a>
+			<a href="https://www.facebook.com/Zenazia-Quint-1811265022491883/">
+				<svg width="25" height="25" viewBox="0 0 25 25">
+					<path d="M13.353 25H1.38A1.38 1.38 0 010 23.62V1.38C0 .618.618 0 1.38 0h22.24C24.382 0 25 .618 25 1.38v22.24A1.38 1.38 0 0123.62 25h-6.37v-9.681h3.25l.486-3.773H17.25v-2.41c0-1.092.303-1.836 1.87-1.836h1.997V3.924c-.345-.046-1.531-.15-2.911-.15-2.88 0-4.853 1.76-4.853 4.988v2.783h-3.258v3.773h3.258V25z" fill="#FFF" fill-rule="evenodd"></path>
+				</svg>
+				<span>Quint-Fonsegrives</span>
+			</a>
+		</div>-->
+		<svg class="logoKSE" id="Lines" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 143.73 285.58"><defs><style>.cls-1{fill:none;stroke:#FFF;stroke-linecap:round;stroke-miterlimit:10;stroke-width:10px;}</style></defs><title>KSE_A2</title><line class="cls-1" x1="1" y1="142.85" x2="142.73" y2="1.12"/><line class="cls-1" x1="1" y1="1.12" x2="1" y2="284.58"/><line class="cls-1" x1="142.73" y1="284.58" x2="1" y2="142.85"/><path class="cls-1" d="M470.87,158.27c-14.54.36-26.43.21-34.71,0-6.84-.18-16.61-.14-36.16,0a71.33,71.33,0,0,0-13.22,1.23,70.83,70.83,0,0,0-28.33,12.22A71.08,71.08,0,0,0,334,203.3a71,71,0,0,0,2.59,57.48,71.83,71.83,0,0,0,22.58,26.27,70.9,70.9,0,0,0,18.6,9.39,61.93,61.93,0,0,0,13,3c.75.08,3.59.26,9.26.6,8.22.5,8.8.5,10.68.8,4.32.7,8.81,1.94,9.16,2,0,0,.68.17,1.29.36a70.91,70.91,0,0,1,11.28,130.7A72.67,72.67,0,0,1,400,441.73" transform="translate(-328.13 -157.15)"/><line class="cls-1" x1="142.73" y1="284.58" x2="1" y2="284.58"/><line class="cls-1" x1="142.73" y1="142.85" x2="1" y2="142.85"/><line class="cls-1" x1="1" y1="1.12" x2="142.73" y2="1.12"/></svg>
+		<p class="copyright"><?php echo the_field("footer_copyright") . ' ' . current_time('Y'); ?></p>
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
+<script>
+	let scrollTopBtn = document.getElementById("scrollToTop");
+	let rootElement = document.documentElement;
 
+	function scrollToTop() {
+		rootElement.scrollTo({
+			top: 0,
+			behavior: "smooth"
+		})
+	}
+	scrollTopBtn.addEventListener("click", scrollToTop);
+</script>
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
--- wp-content/themes/zenazia/template-accueil.php
+++ wp-content/themes/zenazia/template-accueil.php
@@ -2,9 +2,11 @@
 /* Template Name: Accueil */
 get_header();
 ?>
-	<main id="primary" class="site-main">
-	<section id="accueil">
-	    <div class="container">
+<div id="leftdoor"></div>
+<div id="rightdoor"></div>
+<main id="primary" class="site-main">
+    <section id="accueil">
+        <div class="container">
             <div class="bigImage">
                 <div class="menu">
                     <div class="logo">
@@ -12,21 +14,22 @@ get_header();
                     </div>
                     <nav id="site-navigation" class="main-navigation">
                         <?php
-                            wp_nav_menu(
-                                array(
-                                    'theme_location' => 'menu-1',
-                                    'menu_id'        => 'primary-menu',
-                                )
-                            );
+                        wp_nav_menu(
+                            array(
+                                'theme_location' => 'menu-1',
+                                'menu_id'        => 'primary-menu',
+                            )
+                        );
                         ?>
                     </nav>
                 </div>
                 <div class="introduction">
                     <div class="firstIntroText">
-                        <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
-                            the_content();
-                        endwhile; else: ?>
-                        <p>Sorry, no posts matched your criteria.</p>
+                        <?php if (have_posts()) : while (have_posts()) : the_post();
+                                the_content();
+                            endwhile;
+                        else : ?>
+                            <p>Sorry, no posts matched your criteria.</p>
                         <?php endif; ?>
                     </div>
                     <div class="secondIntroText">
@@ -42,23 +45,116 @@ get_header();
                     <p class="subdragonZenazia"><?php echo get_field('subdragonzenazia'); ?></p>
                 </div>
             </div>
-	    </div>
-	    </section>
-	    <section id="restaurants" style="height: 1400px">
-	        <h2>Restaurant de Balma</h2>
-	        <div class="chopsticksImg"></div>
-	        <section id="balma">
-	        	<div class="leftContent">
-	        	    <?php echo get_field('horaires_douverture'); ?>
-	        	</div>
-	        	<div class="rightImg"></div>
-	        </section>
-	        <section id="quint">
-                <?php /* <div class="leftContent">
-                    <?php echo get_field('horaires_douverture'); ?>
-                </div> */ ?>
-                <div class="rightImg"></div>
-            </section>
-	    </section>
-	</main>
+        </div>
+    </section>
+    <section id="restaurants">
+        <div class="chopsticksImg"></div>
+        <section id="balma">
+            <h2>Restaurant de Balma</h2>
+            <div class="balmaContent">
+                <div class="leftContent">
+                    <ul class="timetable">
+                        <?php
+                        if (have_rows('horaires_ouverture_balma')) :
+                            while (have_rows('horaires_ouverture_balma')) : the_row();
+                                $lettre_jour = get_sub_field('lettre_jour');
+                                $horaire_matin = get_sub_field('horaires_matin');
+                                $horaire_apresmidi = get_sub_field('horaires_apres_midi');
+                        ?>
+                                <li class="card-timetable">
+                                    <span class="letter-timetable"><?php echo $lettre_jour; ?></span>
+                                    <div class="hours">
+                                        <span class="time-timetable"><?php echo $horaire_matin; ?></span>
+                                        <span class="time-timetable"><?php echo $horaire_apresmidi; ?></span>
+                                    </div>
+                                </li>
+                        <?php
+                            endwhile;
+                        else :
+                            echo "Pas d'horaires";
+                        endif;
+                        ?>
+                    </ul>
+                </div>
+                <div class="rightContent">
+                    <div class="rightImg"></div>
+                    <div class="rightTxt">
+                        <p class="rightTxtIntro"><?php echo get_field("texte_sous_image_balma"); ?></p>
+                        <a href="tel:<?php echo get_field("telephone_balma_reel"); ?>" class="rightTxtPhone">
+                            <img src="<?php bloginfo('template_url'); ?>/images/phone.svg" width="20px" />
+                            <span><?php echo get_field("telephone_balma"); ?></span>
+                        </a>
+                        <a target="_blank" href="https://www.google.com/maps/place/Zenazia+Toulouse+Balma/@43.6061128,1.4833313,15z/data=!4m2!3m1!1s0x0:0xfc27fd79a5a7a20c?sa=X&ved=2ahUKEwjOvrO5tdvvAhXCzIUKHdCWDnYQ_BIwEnoECC0QBQ" class="rightTxtAddress">
+                            <img src="<?php bloginfo('template_url'); ?>/images/pin.svg" width="20px" />
+                            <span><?php echo get_field("adresse_balma"); ?></span>
+                        </a>
+                        <div class="bottomRightTxt">
+                            <div class="link-services">
+                                <a target="_blank" href="https://www.ubereats.com/fr/toulouse/food-delivery/zenazia/rFbZ9WqgSXGgYxPnIWvHLA"><img src="<?php bloginfo('template_url'); ?>/images/logoUber.svg" width="120px" /></a>
+                                <a target="_blank" href="https://deliveroo.fr/fr/menu/toulouse/roserai-juncasse-argoulets/zenazia-toulouse-balma"><img src="<?php bloginfo('template_url'); ?>/images/logoDeliveroo.svg" width="120px" /></a>
+                            </div>
+                            <div id="TA_cdsratingsonlynarrow907" class="TA_cdsratingsonlynarrow">
+                                <ul id="8mMlRst" class="TA_links W5LYgaC9">
+                                    <li id="Z5R2Qvuai" class="m8lENNOEweOe"><a target="_blank" href="https://www.tripadvisor.fr/Restaurant_Review-g187175-d967665-Reviews-Zenazia-Toulouse_Haute_Garonne_Occitanie.html"><img src="https://www.tripadvisor.fr/img/cdsi/img2/branding/v2/Tripadvisor_lockup_horizontal_secondary_registered-18034-2.svg" alt="TripAdvisor" /></a></li>
+                                </ul>
+                            </div>
+                            <script async src="https://www.jscache.com/wejs?wtype=cdsratingsonlynarrow&amp;uniq=907&amp;locationId=967665&amp;lang=fr&amp;border=true&amp;display_version=2" data-loadtrk onload="this.loadtrk=true"></script>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="quint">
+            <h2>Restaurant de Quint-Fonsegrives</h2>
+            <div class="quintContent">
+                <div class="leftContent">
+                    <div class="leftImg"></div>
+                    <div class="leftTxt">
+                        <p class="leftTxtIntro"><?php echo get_field("texte_sous_image_quint"); ?></p>
+                        <a href="tel:<?php echo get_field("telephone_quint_reel"); ?>" class="leftTxtPhone">
+                            <img src="<?php bloginfo('template_url'); ?>/images/phone.svg" width="20px" />
+                            <span><?php echo get_field("telephone_quint"); ?></span>
+                        </a>
+                        <a target="_blank" href="https://www.google.com/maps/place/Zenazia+Quint/@43.5830595,1.511978,17z/data=!3m1!4b1!4m5!3m4!1s0x12aebd845d4de07b:0xf185b85c048b670f!8m2!3d43.5830556!4d1.5141667" class="leftTxtAddress">
+                            <img src="<?php bloginfo('template_url'); ?>/images/pin.svg" width="20px" />
+                            <span><?php echo get_field("adresse_quint"); ?></span>
+                        </a>
+                        <div class="bottomLeftTxt">
+                            <div class="link-services">
+                                <a target="_blank" href="https://www.ubereats.com/fr/toulouse/food-delivery/zenazia-ii-quint/TQ7c09tFTomxMFJfFwa1cg?pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMjMxJTIwQXZlbnVlJTIwTWVyY3VyZSUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMkNoSUoyeHVNNjRlOXJoSVJ5LW5ENkFoMzdkcyUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJnb29nbGVfcGxhY2VzJTIyJTJDJTIybGF0aXR1ZGUlMjIlM0E0My41ODA2ODklMkMlMjJsb25naXR1ZGUlMjIlM0ExLjUyMTg3NjglN0Q%3D&utm_campaign=place-action-link&utm_medium=organic&utm_source=google"><img src="<?php bloginfo('template_url'); ?>/images/logoUber.svg" width="120px" /></a>
+                            </div>
+                            <div id="TA_cdsratingsonlynarrow964" class="TA_cdsratingsonlynarrow"><ul id="5owhCA7rjvQ" class="TA_links VsLrZXSrt4h"><li id="vk7vZzj" class="eeXrO9kUDfL"><a target="_blank" href="https://www.tripadvisor.fr/Restaurant_Review-g1467813-d4297179-Reviews-Zenazia_2-Quint_Fonsegrives_Haute_Garonne_Occitanie.html"><img src="https://www.tripadvisor.fr/img/cdsi/img2/branding/v2/Tripadvisor_lockup_horizontal_secondary_registered-18034-2.svg" alt="TripAdvisor"/></a></li></ul></div><script async src="https://www.jscache.com/wejs?wtype=cdsratingsonlynarrow&amp;uniq=964&amp;locationId=4297179&amp;lang=fr&amp;border=true&amp;display_version=2" data-loadtrk onload="this.loadtrk=true"></script>
+                        </div>
+                    </div>
+                </div>
+                <div class="rightContent">
+                    <div class="hatImg"></div>
+                    <ul class="timetable">
+                        <?php
+                        if (have_rows('horaires_ouverture_quint')) :
+                            while (have_rows('horaires_ouverture_quint')) : the_row();
+                                $lettre_jour = get_sub_field('lettre_jour');
+                                $horaire_matin = get_sub_field('horaires_matin');
+                                $horaire_apresmidi = get_sub_field('horaires_apres_midi');
+                        ?>
+                                <li class="card-timetable">
+                                    <span class="letter-timetable"><?php echo $lettre_jour; ?></span>
+                                    <div class="hours">
+                                        <span class="time-timetable"><?php echo $horaire_matin; ?></span>
+                                        <span class="time-timetable"><?php echo $horaire_apresmidi; ?></span>
+                                    </div>
+                                </li>
+                        <?php
+                            endwhile;
+                        else :
+                            echo "Pas d'horaires";
+                        endif;
+                        ?>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </section>
+</main>
 <?php
+get_footer();
